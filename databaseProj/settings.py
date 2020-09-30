00f76cc9bb4be932a66dece1e290491e6d264a7f
Greko2017 <33413355+Greko2017@users.noreply.github.com>
2020-09-30 17:03:14 +0100

    fixing collect static deploy error

--- databaseProj/settings.py
+++ databaseProj/settings.py
@@ -1,6 +1,7 @@
 import os
 import json
 
+import django_heroku
 # json_file = open('databaseProj/config_vars.json').read()
 # data = json.loads(json_file)
 # secret = data['SECRET_KEY']
@@ -133,7 +134,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+django_heroku.settings(locals())
