c46d119ff0e141d34117f2aca69835aad18ce7ec
Greko2017 <gregory.goufan@gmail.com>
2020-10-01 13:34:33 +0100

    added static file

--- travellite/migrations/0007_auto_20201001_1318.py
+++ travellite/migrations/0007_auto_20201001_1318.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('travellite', '0006_history_bookingStatus'),
+        ('travellite', '0006_history_booking_status'),
     ]
 
     operations = [
